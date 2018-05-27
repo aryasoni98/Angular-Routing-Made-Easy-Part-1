ad24cacc932a2684e14d68fa993c7cbfe3fc91a2
Arya Soni <aryasoni98@gmail.com>
2018-05-27 01:40:03 -0700

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import { AppComponent } from './app.component';
+import {ListComponent} from './list/list.component';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'items',
+    pathMatch: 'full'
+  },
+  {
+    path: 'items',
+    component: ListComponent
+  }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
